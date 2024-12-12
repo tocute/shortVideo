7151f571fbc21a8cbc549b0f72d0391dfa90663d
Bill.Chang <tocute520@gmail.com>
2024-12-12 15:43:17 +0800

    Update calc.js
    fix bug

--- calc.js
+++ calc.js
@@ -1,5 +1,17 @@
 function calculateSum(a, b) {
+  return a + b;
+}
+
+function calculateMinus(a, b) {
   return a - b;
+}
+
+function calculateMultiply(a, b) {
+  return a * b;
+}
+
+function calculateDivide(a, b) {
+  return a / b;
 }
 
 let sum = calculateSum(5, 10);
